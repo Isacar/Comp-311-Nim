08483f4753c3501765aa58f05319ca8f14c08a40
Isacar <irodrigu@harding.edu>
2014-04-29 20:13:29 -0500

    Working on client_main.cpp

--- NIM/NIM/client_main.cpp
+++ NIM/NIM/client_main.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-using namespace std;
+// client_main.cpp
+//   This function serves as the "main" function for the client-side
 #include "nim.h";
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
 
 void   client_main(int argc, char* argv[] )
 {
 	char clientName[ MAX_SERVICE_SIZE ];
-	cout << "Name?" << endl;
-	cin >> clientName;
+	std::cout << "Name?" << std::endl;
+	std::cin >> clientName;
 
 	SOCKET s = connectsock( BROADCAST_ADDR, UDPPORT_NIM, "udp" );
 	std::string host;
 	std::string port;
 	ServerStruct server[MAX_HOST];
 	int numServers = 0;
-	UDP_send( s, "Name?", MAX_SEND_BUF - 1, BROADCAST_ADDR, UDPPORT_NIM );
+	UDP_send( s, NIM_QUERY, MAX_SEND_BUF - 1, BROADCAST_ADDR, UDPPORT_NIM );
 
 	int status = wait( s, 20, 0 );
 	bool serverFound = false;
@@ -23,12 +26,12 @@ void   client_main(int argc, char* argv[] )
 		serverFound = true;
 		char datagram[MAX_RECV_BUF];
 
-		UDP_recv( s, datagram, MAX_RECV_BUF - 1, server[numServers].host, server[numServers].port );
+		UDP_recv( s, datagram, MAX_RECV_BUF - 1, (char*) server[numServers].host.c_str(), (char*)server[numServers].port.c_str() );
 		if( strncmp( datagram, clientName, 5 ) == 0 )
 		{
 			char* afterPrefix = strstr( datagram, clientName ) + strlen( clientName );
-			strcpy_s( server[numServers].name, afterPrefix );
-
+			strcpy_s(server[numServers].name.c_str(), afterPrefix );
+		
 			numServers++;
 		}
 		else
@@ -44,48 +47,48 @@ void   client_main(int argc, char* argv[] )
 		char datagram [MAX_RECV_BUF];
 		do{
 			
-			cout << "Choose a server. Enter '0' to quit: " << endl;
+			std::cout << "Choose a server. Enter '0' to quit: " << std::endl;
 			for(int i = 0; i < numServers; i++)
 			{
-				cout << i + 1 << ". " << server[i].name << endl;
+				std::cout << i + 1 << ". " << server[i].name << std::endl;
 			}
 		
 			int chosenServerIndex;
-			cin >> chosenServerIndex;
+			std::cin >> chosenServerIndex;
 			if( chosenServerIndex == 0 )
 			{
 				readyToQuit = true;
 			}
 			while( chosenServerIndex > numServers )
 			{
-				cout << "Incorrect input. Choose again." << endl;
-				cin >> chosenServerIndex;
+				std::cout << "Incorrect input. Choose again." << std::endl;
+				std::cin >> chosenServerIndex;
 			}
 			char challenge [1034] = "Play?Name=";
 			strcat( challenge, clientName );
-			UDP_send( s, challenge, MAX_SEND_BUF - 1, server[chosenServerIndex - 1].host, UDPPORT_NIM );
+			UDP_send( s, challenge, MAX_SEND_BUF - 1, (char*)server[chosenServerIndex - 1].host.c_str(), UDPPORT_NIM );
 
 			status = wait( s, 20, 0 );
 
 			if(status > 0)
 			{
-				UDP_recv( s, datagram, MAX_RECV_BUF - 1, server[chosenServerIndex - 1].host, UDPPORT_NIM );
+				UDP_recv( s, datagram, MAX_RECV_BUF - 1, (char*)server[chosenServerIndex - 1].host.c_str(), UDPPORT_NIM );
 				if( strcmp( datagram, "YES" ) == 0 )
 				{
-					UDP_send( s, "GREAT!", MAX_SEND_BUF, server[chosenServerIndex - 1].host, UDPPORT_NIM );
+					UDP_send( s, "GREAT!", MAX_SEND_BUF, (char*)server[chosenServerIndex - 1].host.c_str(), UDPPORT_NIM );
 					closesocket(s);
-					SOCKET s = connectsock( server[chosenServerIndex - 1].host, TCPPORT_NIM, "tcp" );
+					SOCKET s = connectsock( (char*)server[chosenServerIndex - 1].host.c_str(), TCPPORT_NIM, "tcp" );
 					//play_Nim( s, ??, ?? );
 					readyToQuit = true;
 				}
 				else
 				{
-					cout << "Negative response. Try again." << endl;
+					std::cout << "Negative response. Try again." << std::endl;
 				}
 			}
 			else
 			{
-				cout << "Negative response. Try again." << endl;
+				std::cout << "Negative response. Try again." << std::endl;
 			}
 
 		}while( !readyToQuit );
